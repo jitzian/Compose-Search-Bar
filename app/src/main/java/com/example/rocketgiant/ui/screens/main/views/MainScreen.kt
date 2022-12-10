a3ed66696e6ed9ed7f3dd145848004d31a9af734
joNA <jitzian@gmail.com>
2022-12-09 21:26:28 -0500

    rocket jc - First iteration of MainScreen. Still pending to implement the search input and details screen as well
    Cleanup is required

---
+++ app/src/main/java/com/example/rocketgiant/ui/screens/main/views/MainScreen.kt
@@ -0,0 +1,64 @@
+@file:Suppress("FINAL_UPPER_BOUND")
+
+package com.example.rocketgiant.ui.screens.main.views
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.semantics
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.rocketgiant.data.remote.model.Result
+import com.example.rocketgiant.ui.app.RocketScreensApp
+import com.example.rocketgiant.ui.common.*
+import com.example.rocketgiant.ui.screens.main.viewmodel.MainViewModel
+
+@Composable
+fun MainScreenState(mainViewModel: MainViewModel = hiltViewModel()) {
+
+    val state by mainViewModel.state.collectAsState()
+    mainViewModel.fetchGames()
+
+    when (state) {
+        is MainViewModel.UIState.Loading -> {
+            LoadingScreen()
+        }
+        is MainViewModel.UIState.Success -> {
+            MainScreen(data = ((state as MainViewModel.UIState.Success).data))
+        }
+        is MainViewModel.UIState.Error -> {
+            ErrorScreen(message = (state as MainViewModel.UIState.Error).message)
+        }
+    }
+}
+
+@Composable
+fun <T : Result> MainScreen(data: List<T>) {
+    val state = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+    RocketScreensApp {
+        Scaffold(
+            topBar = {
+                MainTopBar(
+                    showBackButton = false,
+                    barTitle = "Search for games" // TODO: Move this to the resource file
+                )
+            },
+            modifier = Modifier
+                .semantics(mergeDescendants = true) {}
+            //.testTag(stringResource(id = R.string.pizzaScreen_test_TAG))
+        ) {
+            LazyColumn(state = state) {
+                items(data) { item ->
+                    RowItem(data = item)
+                }
+            }
+            ScrollFloatingButton(state = state, coroutineScope = scope, data = data)
+        }
+    }
+}
