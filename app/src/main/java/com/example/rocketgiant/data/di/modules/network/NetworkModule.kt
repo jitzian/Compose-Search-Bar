19b5b3657c1d596c4bc88fda8ba7fa33eb1dbfff
joNA <jitzian@gmail.com>
2022-12-11 14:19:51 -0500

    rocket jc - Updating Retrofit instance with logging interceptor to analyze requests

--- app/src/main/java/com/example/rocketgiant/data/di/modules/network/NetworkModule.kt
+++ app/src/main/java/com/example/rocketgiant/data/di/modules/network/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.example.rocketgiant.data.di.modules.network
 
+import com.example.rocketgiant.BuildConfig
 import com.example.rocketgiant.constants.GlobalConstants
 import com.example.rocketgiant.data.remote.RestApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -14,15 +17,30 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
-    //TODO: Check if interceptor is required..!!!
     @Provides
     @Singleton //Scope for instance
     fun providesApi(): RestApi {
         return Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .client(setupOkHttpClient())
             .baseUrl(GlobalConstants.baseUrl)
             .build()
             .create(RestApi::class.java)
+    }
+
+    //Setup OkHttp client and logger interceptor
+    private fun setupOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(httpLoggingInterceptor)
+        httpLoggingInterceptor.setLevel(
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.BASIC
+            }
+        )
+        return httpClient.build()
     }
 
 }
