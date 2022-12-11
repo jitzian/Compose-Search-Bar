4aa869d023c020703baf02bf834b2c28e0a87c7c
joNA <jitzian@gmail.com>
2022-12-11 14:19:16 -0500

    rocket jc - Adding endpoint for game details

--- app/src/main/java/com/example/rocketgiant/data/remote/RestApi.kt
+++ app/src/main/java/com/example/rocketgiant/data/remote/RestApi.kt
@@ -1,10 +1,18 @@
 package com.example.rocketgiant.data.remote
 
+import com.example.rocketgiant.data.remote.model.ResulApiSingleGame
 import com.example.rocketgiant.data.remote.model.ResultApi
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 interface RestApi {
     @GET("games/")
     suspend fun fetchGames(@QueryMap queryMap: Map<String, String>): ResultApi
+
+    @GET("game/{id}/?")
+    suspend fun fetchGameDetailsById(
+        @Path("id") id: String,
+        @QueryMap queryMap: Map<String, String>
+    ): ResulApiSingleGame
 }
