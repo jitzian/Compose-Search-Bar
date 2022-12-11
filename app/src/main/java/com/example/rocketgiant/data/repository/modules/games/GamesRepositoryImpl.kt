95060cc2ca7702533f573bfe86a06d51fe6b8156
joNA <jitzian@gmail.com>
2022-12-11 14:20:42 -0500

    rocket jc - Updating Repository to also include fetchGameDetailsById api

--- app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
+++ app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.rocketgiant.data.repository.modules.games
 
 import com.example.rocketgiant.data.remote.RestApi
+import com.example.rocketgiant.data.remote.model.ResulApiSingleGame
 import com.example.rocketgiant.data.remote.model.ResultApi
 import com.example.rocketgiant.domain.repository.games.GamesRepository
 import javax.inject.Inject
@@ -11,6 +12,16 @@ class GamesRepositoryImpl @Inject constructor(private val restApi: RestApi) : Ga
             mapOf(
                 "api_key" to "9d45436f87d3848d2bdcce810bacb6df57dfd134",
                 "filter" to "name:$gameName",
+                "format" to "json"
+            )
+        )
+    }
+
+    override suspend fun fetchGameDetailsById(id: Int): ResulApiSingleGame {
+        return restApi.fetchGameDetailsById(
+            id = id.toString(),
+            mapOf(
+                "api_key" to "9d45436f87d3848d2bdcce810bacb6df57dfd134",
                 "format" to "json"
             )
         )
--- app/src/main/java/com/example/rocketgiant/domain/repository/games/GamesRepository.kt
+++ app/src/main/java/com/example/rocketgiant/domain/repository/games/GamesRepository.kt
@@ -1,7 +1,9 @@
 package com.example.rocketgiant.domain.repository.games
 
+import com.example.rocketgiant.data.remote.model.ResulApiSingleGame
 import com.example.rocketgiant.data.remote.model.ResultApi
 
 interface GamesRepository {
     suspend fun fetchGames(gameName: String): ResultApi
+    suspend fun fetchGameDetailsById(id: Int): ResulApiSingleGame
 }
