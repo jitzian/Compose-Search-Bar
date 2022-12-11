11dd443d36f7da91ad56efe2ab510b19d309de43
joNA <jitzian@gmail.com>
2022-12-11 18:19:44 -0500

    rocket jc - Doing some cleanup

--- app/src/main/java/com/example/rocketgiant/constants/GlobalConstants.kt
+++ app/src/main/java/com/example/rocketgiant/constants/GlobalConstants.kt
@@ -2,8 +2,14 @@ package com.example.rocketgiant.constants
 
 class GlobalConstants {
     companion object {
+        //EndPoint constants
+        const val API_KEY = ""
         const val baseUrl = "https://www.giantbomb.com/api/"
+
+        //Fetch API general config
         const val MAX_TIMEOUT = 30000L
+
+        //Various stuff
         const val MAX_LINES = 1
     }
 }
--- app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
+++ app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.rocketgiant.data.repository.modules.games
 
+import com.example.rocketgiant.constants.GlobalConstants.Companion.API_KEY
 import com.example.rocketgiant.data.remote.RestApi
 import com.example.rocketgiant.data.remote.model.ResulApiSingleGame
 import com.example.rocketgiant.data.remote.model.ResultApi
@@ -10,7 +11,7 @@ class GamesRepositoryImpl @Inject constructor(private val restApi: RestApi) : Ga
     override suspend fun fetchGames(gameName: String): ResultApi {
         return restApi.fetchGames(
             mapOf(
-                "api_key" to "9d45436f87d3848d2bdcce810bacb6df57dfd134",
+                "api_key" to API_KEY,
                 "filter" to "name:$gameName",
                 "format" to "json"
             )
@@ -21,7 +22,7 @@ class GamesRepositoryImpl @Inject constructor(private val restApi: RestApi) : Ga
         return restApi.fetchGameDetailsById(
             id = id.toString(),
             mapOf(
-                "api_key" to "9d45436f87d3848d2bdcce810bacb6df57dfd134",
+                "api_key" to API_KEY,
                 "format" to "json"
             )
         )
