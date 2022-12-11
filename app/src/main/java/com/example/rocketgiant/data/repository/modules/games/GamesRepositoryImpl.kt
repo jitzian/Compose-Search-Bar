fcf08b928106f2e9dd77edf1ce99a055999c6939
joNA <jitzian@gmail.com>
2022-12-10 19:36:03 -0500

    rocket jc - Correcting query param

--- app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
+++ app/src/main/java/com/example/rocketgiant/data/repository/modules/games/GamesRepositoryImpl.kt
@@ -5,14 +5,12 @@ import com.example.rocketgiant.data.remote.model.ResultApi
 import com.example.rocketgiant.domain.repository.games.GamesRepository
 import javax.inject.Inject
 
-class GamesRepositoryImpl @Inject constructor(private val restApi: RestApi): GamesRepository {
+class GamesRepositoryImpl @Inject constructor(private val restApi: RestApi) : GamesRepository {
     override suspend fun fetchGames(gameName: String): ResultApi {
-        //return restApi.fetchGames(gameName)
-        //https://www.giantbomb.com/api/games/?api_key=9d45436f87d3848d2bdcce810bacb6df57dfd134&filter=name:SEGA&format=json
         return restApi.fetchGames(
             mapOf(
                 "api_key" to "9d45436f87d3848d2bdcce810bacb6df57dfd134",
-                "filter=name" to gameName,
+                "filter" to "name:$gameName",
                 "format" to "json"
             )
         )
